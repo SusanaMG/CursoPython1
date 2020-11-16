cd20db169b142dcec778ec9d6f917c5e48b1f60e
susanamg <susana.marquez@atos.net>
2020-11-16 17:24:55 +0100

    Seccion 5: Fundamentos de Programacion - Variables y Tipos de Datos

---
+++ 02-variables.py
@@ -0,0 +1,34 @@
+# VARIABLES
+
+hola_mundo = 'Hola Mundo'
+Hola_mundo = 'Hola Mundo con Mayúscula'
+
+print(hola_mundo)
+print(Hola_mundo)
+
+nombreCliente = 'Juan' # Camelcase
+NombreCliente = 'Juan' # Pascal Case o Capital Camel Case: para clases o programación orientada a objetos
+nombrecliente = 'Juan' # Flat case
+#nombre-cliente = 'Juan' # Kebab Case o dash case
+nombre_cliente = 'Juan' # Snake Case
+
+# Reto: variables
+libro = 'El origen de las especies'
+comida = 'arroz'
+
+print('Libro: ' + libro)
+print('Comida: ' + comida)
+
+# TIPOS DE DATOS
+
+nombre1 = 'Juan' # String o cadenas de texto
+nombre2 = "Juan"
+likes = 201 # Integers o enteros
+total_pagar = 100.20 # Floats o flotantes
+pagado = True # Boolean True vs. False
+
+# Python tiene tipado dinámico (JavaScript, Ruby, Python, PHP)
+edad = 50
+edad = 51
+edad = "Ninguna"
+print(edad)
---
+++ 03-tipodedatos.py
@@ -0,0 +1,16 @@
+# PRACTICANDO LOS TIPOS DE DATOS
+
+nombre = "Juan Pablo"  
+nombre2 = 'Juan Pablo'
+print(nombre)
+
+edad = 18
+edad = 20 # Reasignar el valor de la variable
+print(edad)
+
+cantidad_a_pagar = 100.50
+print(cantidad_a_pagar)
+
+pagado = True
+print(pagado)
+
